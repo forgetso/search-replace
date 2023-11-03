6da71e664f4860ef1afc7f2733f3d8146830c239
Chris <forgetso86@gmail.com>
2023-11-03 10:33:11 +0000

    Fix for gutenberg wordpress editor (#86)

--- src/constants.ts
+++ src/constants.ts
@@ -9,10 +9,19 @@ export const RICH_TEXT_EDITOR_TINY_MCE: RichTextEditor = {
     editor: { type: SelectorType.id, value: '#tinymce', iframe: false },
     container: { type: SelectorType.class, value: '.mce-edit-area', iframe: true },
 }
-export const RICH_TEXT_EDITOR_GENERIC: RichTextEditor = {
+export const RICH_TEXT_EDITOR_GUTENBERG_TITLE: RichTextEditor = {
+    container: { type: SelectorType.attribute, value: '[name="editor-canvas"]', iframe: true },
     editor: { type: SelectorType.attribute, value: '[role="textbox"]', iframe: false },
 }
-export const RICH_TEXT_EDITORS: RichTextEditor[] = [RICH_TEXT_EDITOR_TINY_MCE, RICH_TEXT_EDITOR_GENERIC]
+export const RICH_TEXT_EDITOR_GUTENBERG_DOCUMENT: RichTextEditor = {
+    container: { type: SelectorType.attribute, value: '[name="editor-canvas"]', iframe: true },
+    editor: { type: SelectorType.attribute, value: '[role="document"]', iframe: false },
+}
+export const RICH_TEXT_EDITORS: RichTextEditor[] = [
+    RICH_TEXT_EDITOR_TINY_MCE,
+    RICH_TEXT_EDITOR_GUTENBERG_TITLE,
+    RICH_TEXT_EDITOR_GUTENBERG_DOCUMENT,
+]
 
 export const HINTS = {
     wordpress6: 'Hint: WordPress 6+ detected. Check "Visible content only?" when editing posts.',
