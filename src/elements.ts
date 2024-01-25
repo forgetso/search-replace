d558546413a6944121ef6388170d545639b9ad12
Chris Taylor <forgetso86@gmail.com>
2024-01-25 17:06:12 +0000

    Try to ignore other iframes

--- src/elements.ts
+++ src/elements.ts
@@ -31,6 +31,8 @@ export function getIframeElements(document: Document, blob = false): HTMLIFrameE
             iframe.src.length &&
             // We don't want to count iframes injected by other chrome extensions
             !iframe.src.startsWith('chrome-extension://') &&
+            // We only want to wait on iframes from the same origin
+            iframe.src.startsWith(window.location.origin) &&
             // We may or may not want blob iframes
             (blob ? isBlobIframe(iframe) : !isBlobIframe(iframe))
     )
