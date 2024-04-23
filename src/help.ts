1db16305e5b7ed26a454a5379ff848915e1f8bdd
RonaldHolm <vengefulriker@gmail.com>
2024-04-23 11:47:09 +0300

    Update Language and Save Instance

--- src/help.ts
+++ src/help.ts
@@ -4,5 +4,5 @@ window.addEventListener('DOMContentLoaded', async function () {
     const langData = await getTranslation()
 
     // Localize HTML elements
-    localizeElements(langData)
+    localizeElements(langData, () => {})
 })
